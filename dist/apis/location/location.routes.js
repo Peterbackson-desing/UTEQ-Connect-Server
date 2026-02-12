c58566d2d0a03c7fa06f159e5dc89c801f1e549b
Peterbackson-desing <2023171040@uteq.edu.mx>
2026-02-12 13:07:40 -0600

    cambios

--- dist/apis/location/location.routes.js
+++
@@ -1,7 +0,0 @@
-// location.routes.ts
-import * as express from 'express';
-import { getLocations, getAllAddresses } from './location.controller.js';
-const router = express.Router();
-router.get('/', getLocations);
-router.get('/getAllAddresses', getAllAddresses);
-export default router;
